b32b99f61ca94340246c5b5f1f14e38263916f91
corn1200 <corn120033@gmail.com>
2020-12-22 11:43:16 +0900

    Initial commit

---
+++ src/test/java/com/kimhs/apis/coupangclone/CoupangCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimhs.apis.coupangclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoupangCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

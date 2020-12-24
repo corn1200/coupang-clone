e6f3a78458cd6e7dfe84f27203ccd4fd2f91fe53
corn1200 <corn120033@gmail.com>
2020-12-24 11:39:36 +0900

    feat: 전체 유저 조회, id 로 조회, email 로 조회, 로그인 정보 확인 기능을 추가한다

--- src/main/java/com/kimhs/apis/coupangclone/service/UserService.java
+++ src/main/java/com/kimhs/apis/coupangclone/service/UserService.java
@@ -1,8 +1,15 @@
 package com.kimhs.apis.coupangclone.service;
 
+import com.kimhs.apis.coupangclone.datamodel.dto.UserDTO;
+import com.kimhs.apis.coupangclone.datamodel.vo.UserLoginCheckVO;
+import com.kimhs.apis.coupangclone.model.User;
 import com.kimhs.apis.coupangclone.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller
 public class UserService {
@@ -11,5 +18,25 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public List<UserDTO> getUsers() {
+        return this.userRepository.findAll().stream()
+                .map(UserDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    public UserDTO getUserById(int userId) throws Exception {
+        Optional<User> searchedUser = this.userRepository.findById(userId);
+        return new UserDTO(searchedUser.orElseThrow(() -> new Exception("해당 유저를 찾지 못하였습니다.")));
+    }
+
+    public UserDTO getUserByEmail(String userEmail) throws Exception {
+        Optional<User> searchedUser = Optional.ofNullable(this.userRepository.findByEmail(userEmail));
+        return new UserDTO(searchedUser.orElseThrow(() -> new Exception("해당 유저를 찾지 못하였습니다.")));
+    }
+
+    public Boolean checkLoginInvalid(UserLoginCheckVO userLoginCheckVO) {
+        return false;
     }
 }

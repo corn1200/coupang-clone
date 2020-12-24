7b27d64ce631eedcde51096542588ff134330003
corn1200 <corn120033@gmail.com>
2020-12-24 11:38:02 +0900

    feat: 이메일로 유저 검색과 로그인 정보 확인하는 기능을 추가한다

--- src/main/java/com/kimhs/apis/coupangclone/repository/UserRepository.java
+++ src/main/java/com/kimhs/apis/coupangclone/repository/UserRepository.java
@@ -1,10 +1,16 @@
 package com.kimhs.apis.coupangclone.repository;
 
 import com.kimhs.apis.coupangclone.model.Product;
+import com.kimhs.apis.coupangclone.model.Sale;
 import com.kimhs.apis.coupangclone.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    public User findByEmail(String email);
+
+    public User findByEmailAndPassword(String email, String password);
 }
